7fb8ac9ddff9349b5bfad8bf26547e1fdbe67664
Diaznantama <89216890+Diaznantama@users.noreply.github.com>
2021-09-17 18:33:44 -0800

    Add files via upload

---
+++ Config/Database.php
@@ -0,0 +1,19 @@
+<?php 
+
+/**
+ * 
+ */
+class Database
+{
+	define('http://localhost/utspl2/public/')
+	var $host = 'localhost';
+	var $uname = 'root';
+	var $pass = '';
+	var $db = 'pl2_uts';
+
+	function connect()
+	{
+		$this->connection = mysqli_connect($this->host,$this->uname,$this->pass,$this->db);
+		return $this->connection;
+	}
+}
